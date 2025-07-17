88ec8c9cbe42d33e3f5b8ba4490fb7be9fa40c92
shaihan11 <alien.shaihan@gmail.com>
2025-07-17 16:25:33 +0600

    2nd timeline start

--- dist/assets/wd-main.js
+++ dist/assets/wd-main.js
@@ -2,8 +2,39 @@
     "use strict";
     jQuery(document).ready(function ($) {
 
-        
+        // product timeline animation
+        $(window).on('scroll', function() {
+            $('.single-timeline').each(function() {
+                var elementTop = $(this).offset().top;
+                var elementHeight = $(this).outerHeight();
+                var windowHeight = $(window).height();
+                var scrollTop = $(window).scrollTop();
 
+                // Calculate when the element is 30% from the bottom of the viewport
+                if (scrollTop + windowHeight >= elementTop + (elementHeight * 5)) {
+                    $(this).addClass('active');
+                } else {
+                    $(this).removeClass('active');
+                }
+            });
+        });
+
+
+        // product 360 rotation on scroll
+        gsap.registerPlugin(ScrollTrigger);
+
+        gsap.to(".moving-product-wrapper img", {
+            rotation: 360,
+            transformOrigin: "center center",
+            ease: "none", 
+            scrollTrigger: {
+                trigger: ".wd-moving-product",
+                start: "top 30%", 
+                end: "bottom center", 
+                scrub: 1.5, 
+                // markers: true 
+            }
+        });
 
 
     });
