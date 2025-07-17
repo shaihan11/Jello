e174d1b4b05cee1a62f522416e38dabef077aa17
shaihan11 <alien.shaihan@gmail.com>
2025-07-17 09:42:58 +0600

    Jello starts

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+    content: ["./dist/**/*.{html,js}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                primary: "var(--primary-font)",
+            },
+            fontSize: {},
+            colors: {
+                // texthover: 'var(--text-hover)',
+            },
+            spacing: {
+                // spacex: 'var(--space-x)',
+            },
+            width: {},
+            maxWidth: {},
+            height: {},
+            screens: {
+                // 'min4k': { 'min': '1441px' },
+                // 'maxxl': { 'max': '1280px' },
+            },
+            zIndex: {},
+            backgroundImage: {
+                // 'home-banner': "url('../img/home-banner.png')",
+            },
+            gridTemplateColumns: {},
+
+
+
+            // shn
+            colors: {
+                'site-primary': 'var(--site-primary-color)',
+                'site-secondary': 'var(--site-secondary-color)',
+            },
+        },
+    },
+    plugins: [
+        //   require('@tailwindcss/forms'),
+    ],
+};
